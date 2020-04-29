dbdad7d304542647327f7993c84b458d9066b46e
Guillem Cordoba <guillem.cordoba@gmail.com>
2020-04-29 17:41:15 +0200

    Setup index

--- ui/src/index.ts
+++ ui/src/index.ts
@@ -1,3 +1,14 @@
 import { MicroOrchestrator } from '@uprtcl/micro-orchestrator'
+import { ApolloClientModule } from '@uprtcl/graphql';
+import { HolochainConnection, HolochainConnectionModule } from '@uprtcl/holochain-provider';
+import { MutualCreditModule } from 'holochain-mutual-credit';
 
+const connection = new HolochainConnection({ host: 'ws://localhost:8888' });
 
+const orchestrator = new MicroOrchestrator();
+
+await orchestrator.loadModules([
+    new HolochainConnectionModule(connection),
+    new ApolloClientModule(),
+    new MutualCreditModule('test-instance')
+]);
