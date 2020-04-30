eba3532a0cab77ee9b6b86d49c75077381fa62b8
Guillem Cordoba <guillem.cordoba@gmail.com>
2020-04-30 15:22:12 +0200

    Dev setup

--- ui/src/index.ts
+++ ui/src/index.ts
@@ -2,13 +2,18 @@ import { MicroOrchestrator } from '@uprtcl/micro-orchestrator'
 import { ApolloClientModule } from '@uprtcl/graphql';
 import { HolochainConnection, HolochainConnectionModule } from '@uprtcl/holochain-provider';
 import { MutualCreditModule } from 'holochain-mutual-credit';
+import { ProfilesModule } from 'holochain-profiles';
 
-const connection = new HolochainConnection({ host: 'ws://localhost:8888' });
+(async function () {
 
-const orchestrator = new MicroOrchestrator();
+    const connection = new HolochainConnection({ host: 'ws://localhost:8888' });
 
-await orchestrator.loadModules([
-    new HolochainConnectionModule(connection),
-    new ApolloClientModule(),
-    new MutualCreditModule('test-instance')
-]);
+    const orchestrator = new MicroOrchestrator();
+
+    await orchestrator.loadModules([
+        new HolochainConnectionModule(connection),
+        new ApolloClientModule(),
+        new MutualCreditModule('mutual-credit-instance'),
+        new ProfilesModule('lobby-instance')
+    ]);
+})()
