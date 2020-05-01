0c06b59ddb98f9835410e3365a870038d149fff5
Guillem Cordoba <guillem.cordoba@gmail.com>
2020-05-01 17:39:33 +0200

    Updated social triangulation

--- ui/src/elements/hccm-home.ts
+++ ui/src/elements/hccm-home.ts
@@ -4,20 +4,58 @@ import '@material/mwc-tab';
 import '@material/mwc-tab-bar';
 import '@material/mwc-top-app-bar-fixed';
 import { sharedStyles } from './sharedStyles';
+import { moduleConnect } from '@uprtcl/micro-orchestrator';
+import { ApolloClientModule } from '@uprtcl/graphql';
+import { ApolloClient, gql } from 'apollo-boost';
 
-export class CMHome extends LitElement {
+export class CMHome extends moduleConnect(LitElement) {
   @property({ type: Number })
-  selectedTabIndex: number = 0;
+  selectedTabIndex: number = 2;
+
+  @property({ type: Boolean })
+  numVouches: number = 0;
 
   static get styles() {
     return sharedStyles;
   }
 
+  async firstUpdated() {
+    const client: ApolloClient<any> = await this.request(
+      ApolloClientModule.bindings.Client
+    );
+
+    const result = await client.query({
+      query: gql`
+        {
+          me {
+            id
+            username
+            numVouches
+          }
+        }
+      `,
+    });
+
+    this.numVouches = result.data.me.numVouches;
+  }
+
+  renderPlaceholder() {
+    return html` <span>
+      You only have ${this.numVouches}, but you need N to enter the network
+    </span>`;
+  }
+
   renderContent() {
+    if (this.selectedTabIndex === 2) {
+      return html`<hcst-agent-list></hcst-agent-list>`;
+    }
+
+    if (this.numVouches === 0) return this.renderPlaceholder();
+
     if (this.selectedTabIndex === 0) {
       return html` <hcmc-transaction-list></hcmc-transaction-list>`;
-    } else if (this.selectedTabIndex === 2) {
-      return html`<hcst-agent-list></hcst-agent-list>`;
+    } else {
+      return html`<hcmc-offer-list></hcmc-offer-list>`;
     }
   }
 
@@ -26,6 +64,8 @@ export class CMHome extends LitElement {
       <div class="column">
         <mwc-top-app-bar-fixed>
           <span slot="title">Holochain community currency</span>
+
+          <mwc-button label="Create offer"></mwc-button>
         </mwc-top-app-bar-fixed>
 
         <mwc-tab-bar
---
+++ ui/src/elements/hccm-transactions.ts
@@ -0,0 +1,16 @@
+import { moduleConnect } from '@uprtcl/micro-orchestrator';
+import { LitElement, html } from 'lit-element';
+import { ApolloClient } from 'apollo-boost';
+import { ApolloClientModule } from '@uprtcl/graphql';
+
+export class CMMyTransactions extends moduleConnect(LitElement) {
+  async firstUpdated() {
+    const client: ApolloClient<any> = await this.request(
+      ApolloClientModule.bindings.Client
+    );
+  }
+
+  render() {
+    return html``
+  }
+}
