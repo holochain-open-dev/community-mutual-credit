c5024fc007da3f2dd63ed41abf9a2b17b2a9732d
Guillem Cordoba <guillem.cordoba@gmail.com>
2020-05-14 12:48:12 +0200

    Added block of non-admin interface

--- ui/src/elements/hccm-app.ts
+++ ui/src/elements/hccm-app.ts
@@ -5,6 +5,10 @@ import { setupRouter } from '../router';
 import { ApolloClientModule } from '@uprtcl/graphql';
 import { ApolloClient, gql } from 'apollo-boost';
 import { Router } from '@vaadin/router';
+import {
+  HolochainConnection,
+  HolochainConnectionModule,
+} from '@uprtcl/holochain-provider';
 
 export class CMApp extends moduleConnect(LitElement) {
   @query('#outlet')
@@ -12,6 +16,8 @@ export class CMApp extends moduleConnect(LitElement) {
 
   @property()
   loading: boolean = true;
+  @property()
+  adminInterface: boolean = false;
 
   static get styles() {
     return [
@@ -35,39 +41,46 @@ export class CMApp extends moduleConnect(LitElement) {
   }
 
   async firstUpdated() {
-    const router = setupRouter(this.outlet);
-
-    const client: ApolloClient<any> = this.request(
-      ApolloClientModule.bindings.Client
+    const connection: HolochainConnection = this.request(
+      HolochainConnectionModule.bindings.HolochainConnection
     );
 
-    const result = await client.query({
-      query: gql`
-        {
-          me {
-            id
-            agent {
+    this.adminInterface = await connection.isAdminInterface();
+
+    if (this.adminInterface) {
+      const router = setupRouter(this.outlet);
+      const client: ApolloClient<any> = this.request(
+        ApolloClientModule.bindings.Client
+      );
+
+      const result = await client.query({
+        query: gql`
+          {
+            me {
               id
-              username
-              isInitialMember
+              agent {
+                id
+                username
+                isInitialMember
+              }
+              hasJoined
             }
-            hasJoined
+            minVouches
           }
-          minVouches
-        }
-      `,
-    });
+        `,
+      });
 
-    const isLogin = router.location.getUrl().includes('login');
-    const hasUsername = result.data.me.agent.username != undefined;
-    const hasJoined = result.data.me.hasJoined;
+      const isLogin = router.location.getUrl().includes('login');
+      const hasUsername = result.data.me.agent.username != undefined;
+      const hasJoined = result.data.me.hasJoined;
 
-    if (!isLogin && !hasUsername) {
-      Router.go('/login');
-    } else if (hasUsername && !hasJoined) {
-      Router.go('/disallowed');
-    } else if (hasUsername) {
-      Router.go('/home');
+      if (!isLogin && !hasUsername) {
+        Router.go('/login');
+      } else if (hasUsername && !hasJoined) {
+        Router.go('/disallowed');
+      } else if (hasUsername) {
+        Router.go('/home');
+      }
     }
 
     this.loading = false;
@@ -77,13 +90,28 @@ export class CMApp extends moduleConnect(LitElement) {
     return html`
       <div
         id="outlet"
-        class=${'shell-container ' + (this.loading ? 'hidden' : '')}
+        class=${'shell-container ' +
+        (this.loading || !this.adminInterface ? 'hidden' : '')}
       ></div>
       ${this.loading
         ? html`<div class="fill column center-content">
             <mwc-circular-progress></mwc-circular-progress>
           </div>`
-        : html``}
+        : this.adminInterface
+        ? html``
+        : html` <div class="background"></div>
+            <div class="fill column center-content">
+              <div style="width: 1000px; text-align: center;">
+                <span style="font-size: 26px; font-weight: bold;"
+                  >Wait! You didn't install the happ with the admin interface
+                  activated: uninstall and install again the happ with the admin
+                  interface activated.<br /><br /></span
+                ><span style="font-size: 22px;">
+                  The admin interface is necessary since this happ needs to
+                  clone the Comunity-Currency DNA when joining its network.
+                </span>
+              </div>
+            </div>`}
     `;
   }
 }
